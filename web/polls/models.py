3c989118abee6453f6c4fecbbb22481e103305b3
herrylove72 <bacuongtr@gmail.com>
2016-06-11 17:07:50 +0700

    create model

--- web/polls/models.py
+++ web/polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Question(BaseModel):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data published')
+
+
+class Choice(BaseModel):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
